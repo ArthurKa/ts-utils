a8cb073a79e8221535c4734c8d9151ce2e732548
ArthurKa <arthur.katruk@gmail.com>
2021-09-15 21:34:09 +0300

    feat: add GuardedType generic type

--- src/index.ts
+++ src/index.ts
@@ -66,3 +66,5 @@ type TypeWithGeneric<T> = T[];
 export type ExtractGeneric<T> = T extends TypeWithGeneric<infer U> ? U : never;
 
 export type GeneratorParams<T> = T extends Generator<infer U, infer M, infer A> ? [U, M, A] : never;
+
+export type GuardedType<T> = T extends (x: any) => x is (infer T) ? T : never;
