e0a719d6f14b9ad638f9014b55de97f6b0b202e2
ArthurKa <arthur.katruk@gmail.com>
2022-07-21 01:37:16 +0300

    feat: change Branded BRAND0..10 to BRAND: { [key]: true }

--- src/branded.ts
+++ src/branded.ts
@@ -1,44 +1,23 @@
 declare const BRAND: unique symbol;
-declare const BRAND2: unique symbol;
-declare const BRAND3: unique symbol;
-declare const BRAND4: unique symbol;
-declare const BRAND5: unique symbol;
-declare const BRAND6: unique symbol;
-declare const BRAND7: unique symbol;
-declare const BRAND8: unique symbol;
-declare const BRAND9: unique symbol;
-declare const BRAND10: unique symbol;
+type BRAND = typeof BRAND;
 
 export declare const WITNESS: unique symbol;
 export type WITNESS = typeof WITNESS;
 
-type RearrangeKeys<T extends { [WITNESS]: unknown }> = T[WITNESS] & {
-  [k in Exclude<keyof T, keyof NonNullable<T[WITNESS]>>]: T[k];
+type RearrangeKeysAndUseWitness<T extends { [WITNESS]: unknown;[BRAND]: unknown }> = T[WITNESS] & {
+  [WITNESS]: T[WITNESS];
+  [BRAND]: {
+    [k in keyof T[BRAND]]: T[BRAND][k];
+  };
 };
 
-export type Branded<T extends unknown, U extends string> = RearrangeKeys<T & (
-  T extends { [BRAND9]: unknown }
-    ? { [BRAND10]: U }
-    : T extends { [BRAND8]: unknown }
-      ? { [BRAND9]: U }
-      : T extends { [BRAND7]: unknown }
-        ? { [BRAND8]: U }
-        : T extends { [BRAND6]: unknown }
-          ? { [BRAND7]: U }
-          : T extends { [BRAND5]: unknown }
-            ? { [BRAND6]: U }
-            : T extends { [BRAND4]: unknown }
-              ? { [BRAND5]: U }
-              : T extends { [BRAND3]: unknown }
-                ? { [BRAND4]: U }
-                : T extends { [BRAND2]: unknown }
-                  ? { [BRAND3]: U }
-                  : T extends { [BRAND]: unknown }
-                    ? { [BRAND2]: U }
-                    : {
-                      [WITNESS]: T;
-                      [BRAND]: U;
-                    }
-)>;
+export type Branded<T extends unknown, U extends string> = RearrangeKeysAndUseWitness<{
+  [WITNESS]: T extends { [WITNESS]: unknown } ? T[WITNESS] : T;
+  [BRAND]: (
+    T extends { [BRAND]: unknown }
+      ? T[BRAND] & { [k in U]: true }
+      : { [k in U]: true }
+  );
+}>;
 
 export type Brand<T extends unknown, U extends string> = Branded<T, U>;
---
+++ tests/types/Branded.ts
@@ -0,0 +1,56 @@
+/* eslint-disable import/no-unresolved */
+
+import { Branded } from 'build';
+
+// Regular branding
+type Integer = Branded<number, 'Integer'>;
+type Natural = Branded<Integer, 'Natural'>;
+type NaturalLess10 = Branded<Natural, 'Less10'>;
+type NaturalLess10Not3 = Branded<NaturalLess10, 'Not3'>;
+
+declare const integer: Integer;
+declare const natural: Natural;
+declare const naturalLess10: NaturalLess10;
+declare const naturalLess10Not3: NaturalLess10Not3;
+
+declare function withInteger(e: Integer): void;
+declare function withNatural(e: Natural): void;
+declare function withNaturalLess10(e: NaturalLess10): void;
+declare function withNaturalLess10Not3(e: NaturalLess10Not3): void;
+
+withInteger(integer);
+withInteger(natural);
+withInteger(naturalLess10);
+withInteger(naturalLess10Not3);
+
+// $ExpectError
+withNatural(integer);
+withNatural(natural);
+withNatural(naturalLess10);
+withNatural(naturalLess10Not3);
+
+// $ExpectError
+withNaturalLess10(integer);
+// $ExpectError
+withNaturalLess10(natural);
+withNaturalLess10(naturalLess10);
+withNaturalLess10(naturalLess10Not3);
+
+// $ExpectError
+withNaturalLess10Not3(integer);
+// $ExpectError
+withNaturalLess10Not3(natural);
+// $ExpectError
+withNaturalLess10Not3(naturalLess10);
+withNaturalLess10Not3(naturalLess10Not3);
+
+// Branding inheritance
+type A = Branded<number, 'A'>;
+type B = Branded<number, 'B'>;
+type AB = Branded<A, 'B'>;
+type BA = Branded<B, 'A'>;
+
+declare function withAB(e: AB): void;
+declare const ba: BA;
+
+withAB(ba);
