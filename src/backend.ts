e4452a156c6fb8a877d0fe91989aa8a2175ff161
ArthurKa <arthur.katruk@gmail.com>
2022-08-02 11:58:59 +0300

    chore: split file structure to backend and all

---
+++ src/backend.ts
@@ -0,0 +1,24 @@
+import fs from 'fs';
+import path from 'path';
+import fetch from 'node-fetch';
+
+export const downloadFile = async (url: string, filePath: string) => {
+  const download = async () => {
+    const { body } = await fetch(url);
+    const fileStream = fs.createWriteStream(filePath);
+
+    return new Promise((resolve, reject) => {
+      body.pipe(fileStream);
+      body.on('error', reject);
+      fileStream.on('finish', resolve);
+      fileStream.on('error', reject);
+    });
+  };
+
+  try {
+    await download();
+  } catch(e) {
+    await fs.promises.mkdir(path.parse(filePath).dir, { recursive: true });
+    await download();
+  }
+};
